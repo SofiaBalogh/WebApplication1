4f68d876c481e4d04918aecd0b39dee232078f75
Balogh Zsófia <sofiabalogh@gmail.com>
2019-09-20 19:38:47 +0200

    Add project files.

---
+++ WebApplication1/Controllers/FilmekController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models; //saját projectem neve, így tudja h hol az osztály!
+
+namespace WebApplication1.Controllers
+{
+    public class FilmekController : Controller
+    {
+        // GET: Filmek/Random
+        public ActionResult Random()
+        {
+            var film = new Film() {Cim="Nincs ilyen film" };
+            return View(film);
+        }
+    }
+}
---
+++ WebApplication1/Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Film
+    {
+        public int Id { set; get; }
+        public string Cim { get; set; }
+
+
+
+
+    }
+}
